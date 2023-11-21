cd3469d6107c75dda013a6d0a9a1f56694fa0a66
Christopher Perry <ozbonus@gmail.com>
2023-11-22 00:14:31 +0800

    FEAT: Settings for two Arduinos.

--- button-box-a/button-box-a.ino
+++ button-box-a/button-box-a.ino
@@ -1,6 +1,25 @@
 #include <Joystick.h>
 
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
+
+// So that index 0 refers to pin 10, etc.
+const int indexToPinOffset = 10;
+
+
+// //////////////////////////
+// Individual Box Settings.
+// Enable only one at a time.
+// //////////////////////////
+
+// Settings for Box 1
+// const int DEVICE_ID = 0x03;
+// const int JOY_OFFSET = 0;
+
+// Settings for Box 2
+const int DEVICE_ID = 0x04;
+const int JOY_OFFSET = 4;
+
+
+Joystick_ Joystick(DEVICE_ID, JOYSTICK_TYPE_GAMEPAD,
   8, 0,                  // Button Count, Hat Switch Count
   false, false, false,   // X and Y, but no Z Axis
   false, false, false,   // No Rx, Ry, or Rz
@@ -9,15 +28,10 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_GAMEPAD,
 
 
 void setup() {
-  pinMode(2, INPUT_PULLUP); // JOY_0
-  pinMode(3, INPUT_PULLUP); // JOY_1
-  pinMode(4, INPUT_PULLUP); // JOY_2
-  pinMode(5, INPUT_PULLUP); // JOY_3
-  // Secondary Box
-  pinMode(6, INPUT_PULLUP); // JOY_4
-  pinMode(7, INPUT_PULLUP); // JOY_5
-  pinMode(8, INPUT_PULLUP); // JOY_6
-  pinMode(9, INPUT_PULLUP); // JOY_7
+  pinMode(10, INPUT_PULLUP); // JOY_0
+  pinMode(11, INPUT_PULLUP); // JOY_1
+  pinMode(12, INPUT_PULLUP); // JOY_2
+  pinMode(13, INPUT_PULLUP); // JOY_3
 
   // Initialize Joystick Library.
   Joystick.begin();
@@ -25,18 +39,14 @@ void setup() {
 
 
 // Last Button State
-int lastButtonState[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-
-
-// So that index 0 refers to pin 2, etc.
-const int indexToPinOffset = 2;
+int lastButtonState[4] = {0, 0, 0, 0};
 
 
 void loop() {
-  for (int index = 0; index < 8; index++) {
+  for (int index = 0; index < 4; index++) {
     int currentButtonState = !digitalRead(index + indexToPinOffset);
     if (currentButtonState != lastButtonState[index]) {
-      Joystick.setButton(index, currentButtonState);
+      Joystick.setButton(index + JOY_OFFSET, currentButtonState);
       lastButtonState[index] = currentButtonState;
     }
   }
